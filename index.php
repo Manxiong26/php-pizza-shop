ae2cf7c28f2791958371daccad05056b99667c24
Maneena Xiong <xiong.maneena@gmail.com>
2021-09-13 19:28:23 -0500

    learned functions on numbers

--- index.php
+++ index.php
@@ -35,14 +35,43 @@ define('NAME', 'Wolfy');
     //function 
 
     //this will find the length of the string
-        echo strlen($name);
+        //echo strlen($name);
    //show the string in all uppercase 
-        echo strtoupper($name);
+        //echo strtoupper($name);
     //show the string in lower case 
-        echo strtolower($name);
+        //echo strtolower($name);
     //replace certain things in a string 
     //everytime there's a M in the string $name it will be replaced by W
-        echo str_replace('M', 'W', $name);
+        //echo str_replace('M', 'W', $name);
+
+//Numbers 
+    //intergers are intergers 
+    //float is decimals
+    $radius = 25;
+    $pi = 3.14;
+
+    //basic operators
+    // -, *, +, /, ** the power of
+
+    //echo $pi * $radius**2;
+    //order of operation ( B arracks I power or D ivison M ultiply A dd S ubtract )
+    //echo 2 * (4 + 9 ) / 3;
+
+    //increments & decrement operators
+    //echo $radius++;
+    //echo $radius;
+
+    //shorthand operators
+    $age *= 10;
+    //echo $age;
+
+    //number function 
+    //brings to the closest number 
+    //echo floor($pi);
+    //move it up to the nearest interger 
+    //echo ceil($pi);
+    //built in pi
+    echo pi();
 
 ?>
 
